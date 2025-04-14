679d1b837ff5837802f0b7352159a8e7091d8e43
joao-rs10 <joaovitorreis1000@gmal.com>
2025-04-14 09:48:41 -0300

    cadastro

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@ var sobrenome = prompt("Insira seu sobrenome: ");
 
 if (nome =="") {
     alert("Nome obrigatório");
+    var nome = prompt("Insira seu nome: ");
 }
 else if (sobrenome=="") {
     alert("Sobrenome obrigatório");
+    var sobrenome = prompt("Insira seu sobrenome: ");
 } else{
     alert("Cadasto realizado! Veja os detalhes no console");
 }
