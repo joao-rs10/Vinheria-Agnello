972670ce25c32047c8af9e8a861be065f1005414
joao-rs10 <joaovitorreis1000@gmal.com>
2025-04-14 09:41:37 -0300

    nome_e_sobrenome

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
-var nome = prompt("Insira seu nome: ")
-var sobrenome = prompt("Insira seu sobrenome: ")
+var nome = prompt("Insira seu nome: ");
+var sobrenome = prompt("Insira seu sobrenome: ");
+
+if (nome =="") {
+    alert("Nome obrigatório");
+}
+else if (sobrenome=="") {
+    alert("Sobrenome obrigatório");
+} else{
+    alert("Cadasto realizado! Veja os detalhes no console");
+}
+console.log("Nome do usuário: " + nome)
+console.log("Sobrenome do usuário: " + sobrenome)
+   
