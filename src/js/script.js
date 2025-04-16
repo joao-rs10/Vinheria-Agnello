7dbfe332fc1666f5a3a4aaadc115893c368c4565
Victor Henry <159193296+vhenrym@users.noreply.github.com>
2025-04-16 14:54:28 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,32 @@ if (nomeVinho =="") {
 } else if (tipoVinho=="") {
     alert("Tipo de vinho obrigatório");
     var sobrenome = prompt("Insira o tipo de vinho: ");
+    
+} else if (safra=="") {
+    alert("Safra(ano) do vinho obrigatório.");
+    var safra = prompt("Insira o ano do vinho: ");
+} else if (safra >= 1996) {
+    alert("Esse vinho é considerado jovem.")
+}
+else if (safra >= 2019) {
+    alert("Esse vinho é considerado novo.")
+}
+else if (safra <= 1995) {
+    alert("Esse vinho é considerado velho.")
+}
+
+if (quantidade=="") {
+    alert("Selecione a quantidade de vinhos.");
+    var quantidade = prompt("Insira a quantidade de vinhos: ");
+} else if (quantidade >= 6) {
+    alert("Não é possível selecionar mais de 6 vinhos dessa unidade.")
+    var quantidade = prompt("Selecione até 6 vinhos.")
+}
+
+else {
+    alert("As informações foram concluídas! Confira no console.")
+}
+console.log("Nome do vinho: " + nomeVinho);
+console.log("Tipo do vinho: " + tipoVinho);
+console.log("Safra(ano) do vinho: " + safra);
+console.log("Quantidade de vinho(s): " + quantidade);
