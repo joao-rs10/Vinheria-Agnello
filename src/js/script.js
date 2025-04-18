60aab7603d9e3913fdac2493966bcd4ec7a3829a
williamqbr <williamq2006@gmail.com>
2025-04-18 13:24:55 -0300

    correção

--- src/js/script.js
+++ src/js/script.js
@@ -15,22 +15,24 @@ console.log("Nome do usuário: " + nome);
 console.log("Sobrenome do usuário: " + sobrenome);
 
 var nomeVinho = prompt(" Digite o nome do vinho: ");
-var tipoVinho = prompt(" Digite o tipo de vinho: ");
-var safra = prompt(" Digite a qual a safra(ano): ");
-var quantidade = prompt("Digite a quantidade: ");
-
 if (nomeVinho =="") {
-    alert("Nome do vinho obrigatório");
+    alert("Nome do vinho obrigatório"); }
     var nomeVinho= prompt("Insira o nome do vinho: ");
-} else if (tipoVinho=="") {
+var tipoVinho = prompt(" Digite o tipo de vinho: ");
+if (tipoVinho=="") {
     alert("Tipo de vinho obrigatório");
-    var tipovinho = prompt("Insira o tipo de vinho: ");
-    
-} else if (safra=="") {
+    var tipovinho = prompt("Insira o tipo de vinho: "); }
+var safra = prompt(" Digite a qual a safra(ano): ");
+if (safra=="") {
     alert("Safra(ano) do vinho obrigatório.");
-    var safra = prompt("Insira o ano do vinho: ");
+    var safra = prompt("Insira o ano do vinho: "); }
+var quantidade = prompt("Digite a quantidade: ");
+if (quantidade=="") {
+    alert("Selecione a quantidade de vinhos.");
+    var quantidade = prompt("Insira a quantidade de vinhos: "); }
 
-} else if (safra >= 2020) {
+
+if (safra >= 2020) {
     alert("Esse vinho é considerado jovem.")
     console.log("Safra(ano) do vinho: " + safra + " (VINHO JOVEM)");
 }
@@ -38,15 +40,11 @@ else if (safra >= 2015) {
     alert("Esse vinho é considerado amadurecido.")
     console.log("Safra(ano) do vinho: " + safra + " (VINHO AMADURECIDO)");
 }
-else if (safra <= 2015) {
+else if (safra < 2015) {
     alert("Esse vinho é considerado antigo.")
     console.log("Safra(ano) do vinho: " + safra + " (VINHO ANTIGO)");
 }
 
-if (quantidade=="") {
-    alert("Selecione a quantidade de vinhos.");
-    var quantidade = prompt("Insira a quantidade de vinhos: ");
-}
 
 else {
     alert("As informações foram coletadas! Confira no console.")
